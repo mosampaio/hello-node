37d69c9aa6a6869fa0f131d1c7aecec51cb4493c
mosampaio <mosampaio@gmail.com>
2017-03-17 10:42:03 +1100

    2

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var random = process.env.RANDOM_HOST || 'http://localhost:8000';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
-  request('http://random-svc:8000/random/cities', function (error, response, body) {
+  console.log(random);
+  request(random + '/random/cities', function (error, response, body) {
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     console.log('body:', body); // Print the HTML for the Google homepage.
     res.render('index', { title: body });
   });
-
-  
 });
 
 module.exports = router;
