09c0d1c17db76abd6da99eaf1d240495d40701a1
mosampaio <mosampaio@gmail.com>
2017-03-17 10:23:01 +1100

    Consume random svc

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  request('http://random-svc:8000/random/cities', function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    res.render('index', { title: body });
+  });
+
+  
 });
 
 module.exports = router;
